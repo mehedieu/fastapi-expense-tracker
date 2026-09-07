77de60ad896c97efb2a068943c70489daf4a94ed
mehedieu <mehedi.hassan1248@gmail.com>
2026-09-07 11:46:14 +0600

    requirements.txt added

--- main.py
+++ main.py
@@ -117,5 +117,5 @@ def delete_expense(expense_id : str):
     data = load_data()
     if expense_id not in data:
         raise HTTPException(status_code=404, detail='Expense not found')
-    del data[expense_id]
+    del git add README.mddata[expense_id]
     save_data(data)
